d2ceec7aa3788074da440b024046e779a305a95c
Choi Tae gyu <suker800@gmail.com>
2022-11-07 22:02:37 +0900

    fix :
    안쓰는 Infra 클래스 삭제

--- src/main/java/com/todayhouse/infra/S3Storage/exception/InvalidFileExtensionException.java
+++
@@ -1,9 +0,0 @@
-package com.todayhouse.infra.S3Storage.exception;
-
-import com.todayhouse.global.error.BaseException;
-import com.todayhouse.global.error.BaseResponseStatus;
-
-public class InvalidFileExtensionException extends BaseException {
-
-    public InvalidFileExtensionException(BaseResponseStatus status) { super(status); }
-}
